3ef753cd3b887d01546a045aa6cb82474aef6c42
vesarekola9-ctrl <vesarekola9@gmail.com>
2026-02-25 15:17:31 +0200

    Update BUILD_EXE.bat

--- BUILD_EXE.bat
+++ BUILD_EXE.bat
@@ -12,18 +12,33 @@ python -m pip install -r requirements.txt
 python -m pip install pyinstaller
 
 if exist build rmdir /s /q build
-if exist dist rmdir /s /q dist
+if exist dist_console rmdir /s /q dist_console
+if exist dist_gui rmdir /s /q dist_gui
 if exist TamaCoreBot.spec del /q TamaCoreBot.spec
+if exist TamaCoreBotGUI.spec del /q TamaCoreBotGUI.spec
 
+REM Build console exe (debug)
 python -m PyInstaller ^
   --clean ^
   --onefile ^
   --name TamaCoreBot ^
+  --distpath dist_console ^
   --add-data "tools;tools" ^
   run_pipeline.py
 
+REM Build GUI exe (no console)
+python -m PyInstaller ^
+  --clean ^
+  --onefile ^
+  --noconsole ^
+  --name TamaCoreBotGUI ^
+  --distpath dist_gui ^
+  --add-data "tools;tools" ^
+  gui_app.py
+
 echo.
-echo EXE ready: %cd%\dist\TamaCoreBot.exe
-echo Tip: You can copy TamaCoreBot.exe anywhere (Desktop/USB) and it will create input/output next to it.
+echo DONE:
+echo - %cd%\dist_console\TamaCoreBot.exe
+echo - %cd%\dist_gui\TamaCoreBotGUI.exe
 echo.
 pause
