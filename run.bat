c774c8e401cb9dc095fc0a79e0ed32a3fa329f02
vesarekola9-ctrl <vesarekola9@gmail.com>
2026-03-07 22:37:54 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -11,20 +11,14 @@ call .\.venv\Scripts\activate.bat
 python -m pip install -U pip
 python -m pip install -e .
 
-python -m tamacore.cli inspect-pack --pack assets/packs/demo_pack
+python -m tamacore.cli make-game --v31 --v32 --pack assets/packs/demo_pack --template templates/gdevelop_template --out ..\tamacore-game --with-demo-layout
 if errorlevel 1 goto :fail
 
-python -m tamacore.cli build --v31 --v32 --pack assets/packs/demo_pack --template templates/gdevelop_template --out ..\tamacore-game --with-demo-layout
-if errorlevel 1 goto :fail
-
-python -m tamacore.cli validate --game-dir ..\tamacore-game
-if errorlevel 1 goto :fail
-
-echo [OK] Build complete
+echo [OK] Make Game complete
 goto :end
 
 :fail
-echo [ERR] Build failed
+echo [ERR] Make Game failed
 
 :end
 endlocal
