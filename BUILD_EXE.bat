ca8e3065e6bd86eed09efbe69df84e9ac5197abe
vesarekola9-ctrl <vesarekola9@gmail.com>
2026-03-07 20:02:51 +0200

    Update BUILD_EXE.bat

--- BUILD_EXE.bat
+++ BUILD_EXE.bat
@@ -1,44 +1,15 @@
 @echo off
-cd /d "%~dp0"
+setlocal
 
-if not exist ".venv" (
-  py -m venv .venv
+cd /d %~dp0
+
+if not exist .venv (
+  py -3.12 -m venv .venv
 )
 
-call .venv\Scripts\activate
+call .\.venv\Scripts\activate.bat
+python -m pip install -U pip
+python -m pip install -e .
 
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-python -m pip install pyinstaller
-
-if exist build rmdir /s /q build
-if exist dist_console rmdir /s /q dist_console
-if exist dist_gui rmdir /s /q dist_gui
-if exist TamaCoreBot.spec del /q TamaCoreBot.spec
-if exist TamaCoreBotGUI.spec del /q TamaCoreBotGUI.spec
-
-REM Build console exe (debug)
-python -m PyInstaller ^
-  --clean ^
-  --onefile ^
-  --name TamaCoreBot ^
-  --distpath dist_console ^
-  --add-data "tools;tools" ^
-  run_pipeline.py
-
-REM Build GUI exe (no console)
-python -m PyInstaller ^
-  --clean ^
-  --onefile ^
-  --noconsole ^
-  --name TamaCoreBotGUI ^
-  --distpath dist_gui ^
-  --add-data "tools;tools" ^
-  gui_app.py
-
-echo.
-echo DONE:
-echo - %cd%\dist_console\TamaCoreBot.exe
-echo - %cd%\dist_gui\TamaCoreBotGUI.exe
-echo.
+echo [OK] TamaCore installed into venv
 pause
